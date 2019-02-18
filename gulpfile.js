07826147e436c2faf81208c170e1d855c06b1712
mawa95 <mawa95@gmail.com>
2019-02-18 10:41:03 +0100

    done structure

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('sass', function() {
+    return gulp.src("scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'expanded',
+            sourceComments: 'map'
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/**/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['serve']);
